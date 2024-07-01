f024a66ebef3998eab784079644d8fc3a1ef29a0
Kirill Makshinsky <mky1964@mail.ru>
2024-07-01 10:14:55 +0300

    tests were added

---
+++ constants.py
@@ -0,0 +1,4 @@
+class Constants:
+    EMAIL = 'kirillmakshinskykag7123@yandex.ru' #login зарегистрированного пользователя
+    PASSWORD = '123456' #Пароль зарегистрированного пользователя
+    URL = 'https://stellarburgers.nomoreparties.site/' #URL сайта
